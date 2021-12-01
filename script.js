ef9a3a7c4f53b40349b563213d6f92dc7e644646
nikldev0 <nikhil.kanukuntla@protonmail.com>
2021-12-01 16:12:48 +0000

    set dummy app

--- script.js
+++ script.js
@@ -2,43 +2,43 @@
  * @summary Send events to Event Grid using the Event Grid Schema.
  */
 
+import { EventGridPublisherClient, AzureKeyCredential } from "@azure/eventgrid";
 
-const eventGridKey = "3SErw+mCGtyvTWcVsa1lqd4iJiFW2IsUNmothj75J1A=";
-const apiEndpoint = "https://egtester.uksouth-1.eventgrid.azure.net/api";
+const endpoint = "https://egtester.uksouth-1.eventgrid.azure.net/api";
+const accessKey = "3SErw+mCGtyvTWcVsa1lqd4iJiFW2IsUNmothj75J1A=";
 
-const eventGridApi = axios.create({
-  baseURL: apiEndpoint,
-});
+// const eventGridKey = "3SErw+mCGtyvTWcVsa1lqd4iJiFW2IsUNmothj75J1A=";
+// const apiEndpoint = "https://egtester.uksouth-1.eventgrid.azure.net/api";
 
-function onSubmit() {
-  let postData = {
-    id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-    eventType: "recordInserted",
-    subject: "egtester/submission",
-    eventTime: "2021-11-29T16:18:38.532Z",
-    data: {
-      preferredName: "Nikhil",
-      familyName: "Kanukuntla",
-      accepted: true,
-      message: "Test Message",
-      email: "nikhil.kanukuntla@nhq.com",
-    },
-  };
+async function main() {
+  // Create the client used to publish events to the Event Grid Service
+  const client = new EventGridPublisherClient(
+    endpoint,
+    "EventGrid",
+    new AzureKeyCredential(accessKey)
+  );
 
-  let axiosConfig = {
-    headers: {
-      "Content-Type": "application/json;charset=UTF-8",
-      "aeg-sas-key": eventGridKey,
-      "Access-Control-Allow-Origin": "*",
-    },
-  };
+  // Send an event to the Event Grid Service, using the Event Grid schema.
+  // A random ID will be generated for this event, since one is not provided.
+  await client.send([
+    [
+      {
+        id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+        eventType: "recordInserted",
+        subject: "eventgrid-tester/egtester",
+        eventTime: "2021-11-29T16:18:38.532Z",
+        data: {
+          preferredName: "Nikhil",
+          familyName: "Kanukuntla",
+          accepted: true,
+          message: "This is a message under 1000 characters.",
+          email: "nikhil.kanukuntla@nightingalehq.ai",
+        }
+      },
+    ],
+  ]);
+}
 
-  eventGridApi
-    .post("/events", postData, axiosConfig)
-    .then((result) => {
-      console.log(JSON.stringify(result));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+main().catch(err) => {
+    console.error("The data submitted has encountered an error:", err)
 }
